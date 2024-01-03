2297cd0b33a9bbf4e96e57ec34851856af003c80
“nishanthan-k” <krn.nishanthan@gmail.com>
2024-01-03 13:04:38 +0530

    add dynamic car slider

---
+++ src/sliders/Slider1/Slider1.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import './Slider1.scss';
+
+const Slider1 = () => {
+  const properties = {
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  };
+
+  return (
+    <div className='slider-container'>
+      <h1>Slider</h1>
+      <Slider { ...properties } className='slider'>
+        <div className='img-container primary'>
+          <img
+            src={ require("../../assets/images/car1.jpg") }
+            alt='img'
+          />
+        </div>
+        <div className='img-container secondary'>
+          <img
+            src={ require("../../assets/images/car2.jpeg") }
+            alt='img'
+          />
+        </div>
+        <div className='img-container primary'>
+          <img
+            src={ require("../../assets/images/car3.jpeg") }
+            alt='img'
+          />
+        </div>
+        <div className='img-container secondary'>
+          <img
+            src={ require("../../assets/images/car4.jpeg") }
+            alt='img'
+          />
+        </div>
+        <div className='img-container primary'>
+          <img
+            src={ require("../../assets/images/car5.jpg") }
+            alt='img'
+          />
+        </div>
+        <div className='img-container secondary'>
+          <img
+            src={ require("../../assets/images/car6.jpeg") }
+            alt='img'
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Slider1;
